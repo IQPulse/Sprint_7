f949bc0930bbdc72409c5e1ad17aae5b772d3721
Anton Nazarov <iwebcod@ya.ru>
2024-04-04 13:45:45 +0300

    commit_04.04.2024 №1

--- api/api_client.py
+++ api/api_client.py
@@ -1,10 +1,12 @@
 import requests
+import allure
 from utils.constants import BASE_URL
 
 class APIClient:
     def __init__(self):
         self.base_url = BASE_URL
 
+    @allure.step("Create courier")
     def create_courier(self, login, password, first_name):
         url = f"{self.base_url}/courier"
         data = {
@@ -15,16 +17,19 @@ class APIClient:
         response = requests.post(url, json=data)
         return response
 
+    @allure.step("Delete courier")
     def delete_courier(self, courier_id):
         url = f"{self.base_url}/courier/{courier_id}"
         response = requests.delete(url)
         return response
 
+    @allure.step("Create courier without login or password")
     def create_courier_without_login_or_password(self, data):
         url = f"{self.base_url}/courier"
         response = requests.post(url, json=data)
         return response
 
+    @allure.step("Courier login")
     def courier_login(self, login, password):
         url = f"{self.base_url}/courier/login"
         data = {
@@ -34,11 +39,13 @@ class APIClient:
         response = requests.post(url, json=data)
         return response
 
+    @allure.step("Create order")
     def create_order(self, order_data):
         url = f"{self.base_url}/orders"
         response = requests.post(url, json=order_data)
         return response
 
+    @allure.step("Get orders")
     def get_orders(self):
         url = f"{self.base_url}/orders"
         response = requests.get(url)
