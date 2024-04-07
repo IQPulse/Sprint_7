118d808e7bbc954d9d8b8bba49dd41a7b189dc34
Anton Nazarov <iwebcod@ya.ru>
2024-04-07 11:17:12 +0300

    commit_07.04.2024 №3

--- conftest.py
+++ conftest.py
@@ -4,3 +4,10 @@ from api.api_client import APIClient
 @pytest.fixture(scope="class")
 def api_client():
     return APIClient()
+
+
+@pytest.fixture(scope="class")
+def required_fields(self):
+    return ["id", "firstName", "lastName", "address", "metroStation", "phone",
+            "rentTime", "deliveryDate", "track", "color", "comment", "createdAt",
+            "updatedAt", "status"]
--- tests/test_order_list.py
+++ tests/test_order_list.py
@@ -1,4 +1,3 @@
-import pytest
 import allure
 
 
@@ -6,19 +5,16 @@ import allure
 @allure.story("Проверка списка заказов")
 class TestOrderList:
 
-    @pytest.fixture(scope="class")
-    def required_fields(self):
-        return ["id", "firstName", "lastName", "address", "metroStation", "phone",
-                "rentTime", "deliveryDate", "track", "color", "comment", "createdAt",
-                "updatedAt", "status"]
-
     @allure.title("Check presence of required fields in orders")
-    def test_get_orders(self, api_client, required_fields):
+    def test_get_orders(self, api_client):
+        required_fields = ["id", "firstName", "lastName", "address", "metroStation", "phone",
+                           "rentTime", "deliveryDate", "track", "color", "comment", "createdAt",
+                           "updatedAt", "status"]
+
         with allure.step("Send request to retrieve orders"):
             response = api_client.get_orders()
             orders = response.json().get("orders", [])
 
         with allure.step("Check presence of required fields"):
-            assert all(field in order for field in required_fields for order in orders), \
-                f"One or more required fields missing in orders: {required_fields}"
-
+            missing_fields = [field for field in required_fields if field not in orders[0]]
+            assert not missing_fields
